817b15dfdd059e95b4a815202f20f76485d9b396
robbykraft <robbykraft@gmail.com>
2016-09-19 01:29:40 -0500

    2D HUD

--- Scale2D/scale.c
+++ Scale2D/scale.c
@@ -2,46 +2,97 @@
 
 #define ZOOM_RADIX 3
 
+GLuint texture;
+
+float zoom = 1.0;
+float lvlPercent;
+
+float SPEED = 0.2;
+
+static double unused;
+
 void repeating2DScene(){
 	drawRect(-WIDTH/3*1.5, -HEIGHT*.25, 0, WIDTH/3.0, HEIGHT*.25);
 	drawRect(WIDTH/3*0.5, -HEIGHT*.25, 0, WIDTH/3.0, HEIGHT*.25);
 }
 
-void setup(){ }
-void update(){ }
+void drawHUD(){
+	float thirdW = WIDTH * 0.33;
+	// HUD
+	glColor3f(1.0, 1.0, 1.0);
+	char zoomReport2[50];
+	sprintf(zoomReport2, "LINEAR (X): %.2f (%.2f)", lvlPercent, originY*SPEED);
+	text(zoomReport2, 4, 18, 0);
+	char zoomReport[50];
+	sprintf(zoomReport, "3 ^ X: %.2f", zoom);
+	text(zoomReport, 4, 37, 0);
+
+	glColor3f(1.0, 1.0, 1.0);
+	text("1.0", thirdW*1.5 - 5, 18, 0);
+	text("2.0", thirdW*1.5 - 5, 37, 0);
+
+	glColor3f(1.0, 1.0, 1.0);
+	drawRect(thirdW, 6, 0, thirdW*lvlPercent, 15);
+	drawRect(thirdW, 25, 0, thirdW*(zoom-1) / 2.0, 15);
+
+	glColor3f(0.3, 0.3, 0.3);
+	drawRect(thirdW, 6, 0, thirdW, 15);
+	drawRect(thirdW, 25, 0, thirdW, 15);
+
+	glColor3f(1.0, 1.0, 1.0);
+	text("0.0", thirdW - 30, 18, 0);
+	text("1.0", thirdW - 30, 37, 0);
+	text("1.0", thirdW*2 + 5, 18, 0);
+	text("3.0", thirdW*2 + 5, 37, 0);
+
+}
+
+void setup(){ 
+	// texture = loadTexture("../bin/texture.raw", 32, 32);
+	texture = loadTexture("../bin/stripes512-256.raw", 512, 256);
+}
+void update(){ 
+	lvlPercent = modf(originY * SPEED, &unused);
+	zoom = powf(3, lvlPercent);
+}
 void draw3D(){ 
 
 }
+
 void draw2D(){
-	glTranslatef(WIDTH*0.5, HEIGHT*0.5, 0.0f);
+
+	drawHUD();
+
+	// GROUND
+	glColor3f(1.0, 1.0, 1.0);
+	glTranslatef(WIDTH*0.5, HEIGHT*0.75, 0.0f);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	drawRect(-WIDTH*0.5, 0, 0, WIDTH, WIDTH*0.5);
+	glBindTexture (GL_TEXTURE_2D, 0);
+
 	glPushMatrix();
-		char locationString[50];
-		sprintf(locationString, "%d, %d, %d", (int)originX, (int)originY, (int)originZ );
-		text(locationString, 4, 18, 0);
-		glColor3f(1.0, 1.0, 1.0);
-		drawRect(-WIDTH*0.5, 0, 0, WIDTH, HEIGHT*0.5);
 
-		glPushMatrix();
-		for(int i = 0; i <= 4; i++){
-			float scale = 3;
-			glScalef(1.0/scale, 1.0/scale, 1.0);
-			repeating2DScene();
-		}
-		glPopMatrix();
+		glScalef(zoom, zoom, zoom);
 
-		for(int i = 0; i <= 4; i++){
+		int iterations = 7;
+
+		// subdivisions of space, zooming will repeat on the center one
+		//    eg. ternary cantor set would be 3
+
+		int INTERVAL = 3; 
+		for(int i = 0; i < iterations; i++){
 			glPushMatrix();
-				float scale = powf(3, i);
-				glScalef(1.0/scale, 1.0/scale, 1.0);
-				repeating2DScene();
+			float scale = powf(INTERVAL, i);
+			float color = (i-lvlPercent)/iterations;
+			glColor3f(color*0.75 + 0.25, color*0.75 + 0.25, color*0.75 + 0.25);
+			glScalef(1.0/scale, 1.0/scale, 1.0/scale);
+			repeating2DScene();
 			glPopMatrix();
 		}
 
 	glPopMatrix();
 }
-void keyDown(unsigned int key){
-
-}
+void keyDown(unsigned int key){ }
 void keyUp(unsigned int key){ }
 void mouseDown(unsigned int button){ }
 void mouseUp(unsigned int button){ }
--- headers/world.h
+++ headers/world.h
@@ -531,8 +531,8 @@ GLuint loadTexture(const char * filename, int width, int height){
 	glBindTexture(GL_TEXTURE_2D, texture);
 	// glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 	// glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-	// glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
