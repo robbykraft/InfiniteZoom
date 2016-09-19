f6dfb21013b345967ce96fb487b9cad252e33bd3
robbykraft <robbykraft@gmail.com>
2016-09-19 12:18:56 -0500

    generalized form

--- Scale2D/scale.c
+++ Scale2D/scale.c
@@ -1,39 +1,41 @@
 #include "../headers/world.h"
 
-#define ZOOM_RADIX 3
+#define INTERVAL 3
 
 GLuint texture;
 
-float zoom = 1.0;
-float lvlPercent;
+float zoomCycle = 1.0;
+float linearCycle;
 
 float SPEED = 0.2;
 
 static double unused;
 
 void repeating2DScene(){
-	drawRect(-WIDTH/3*1.5, -HEIGHT*.25, 0, WIDTH/3.0, HEIGHT*.25);
-	drawRect(WIDTH/3*0.5, -HEIGHT*.25, 0, WIDTH/3.0, HEIGHT*.25);
+	float spaceW = (float)WIDTH/INTERVAL;
+	float barW = (WIDTH-INTERVAL)*0.5;
+	drawRect(-barW-spaceW*0.5, -HEIGHT*0.25, 0, barW, HEIGHT*0.25);
+	drawRect(+spaceW*0.5, -HEIGHT*0.25, 0, barW, HEIGHT*0.25);
 }
 
 void drawHUD(){
 	float thirdW = WIDTH * 0.33;
 	// HUD
 	glColor3f(1.0, 1.0, 1.0);
-	char zoomReport2[50];
-	sprintf(zoomReport2, "LINEAR (X): %.2f (%.2f)", lvlPercent, originY*SPEED);
-	text(zoomReport2, 4, 18, 0);
-	char zoomReport[50];
-	sprintf(zoomReport, "3 ^ X: %.2f", zoom);
-	text(zoomReport, 4, 37, 0);
+	char zoomReport[50], zoomReport2[50], oneMinusInterval[50], intervalAsFloat[50];
+	sprintf(zoomReport, "LINEAR (X): %.2f (%.2f)", linearCycle, originY*SPEED);
+	text(zoomReport, 4, 18, 0);
+	sprintf(zoomReport2, "%d ^ X: %.2f", INTERVAL, zoomCycle);
+	text(zoomReport2, 4, 37, 0);
 
 	glColor3f(1.0, 1.0, 1.0);
 	text("1.0", thirdW*1.5 - 5, 18, 0);
-	text("2.0", thirdW*1.5 - 5, 37, 0);
+	sprintf(oneMinusInterval, "%.1f", (float)INTERVAL-1.0);
+	text(oneMinusInterval, thirdW*1.5 - 5, 37, 0);
 
 	glColor3f(1.0, 1.0, 1.0);
-	drawRect(thirdW, 6, 0, thirdW*lvlPercent, 15);
-	drawRect(thirdW, 25, 0, thirdW*(zoom-1) / 2.0, 15);
+	drawRect(thirdW, 6, 0, thirdW*linearCycle, 15);
+	drawRect(thirdW, 25, 0, thirdW*(zoomCycle-1) / (INTERVAL-1), 15);
 
 	glColor3f(0.3, 0.3, 0.3);
 	drawRect(thirdW, 6, 0, thirdW, 15);
@@ -43,21 +45,22 @@ void drawHUD(){
 	text("0.0", thirdW - 30, 18, 0);
 	text("1.0", thirdW - 30, 37, 0);
 	text("1.0", thirdW*2 + 5, 18, 0);
-	text("3.0", thirdW*2 + 5, 37, 0);
-
+	sprintf(intervalAsFloat, "%d.0", INTERVAL);
+	text(intervalAsFloat, thirdW*2 + 5, 37, 0);
 }
 
 void setup(){ 
 	// texture = loadTexture("../bin/texture.raw", 32, 32);
 	texture = loadTexture("../bin/stripes512-256.raw", 512, 256);
 }
-void update(){ 
-	lvlPercent = modf(originY * SPEED, &unused);
-	zoom = powf(3, lvlPercent);
-}
-void draw3D(){ 
 
+void update(){ 
+	float increasing = originY * SPEED;
+	linearCycle = modf(increasing, &unused);
+	zoomCycle = powf(INTERVAL, linearCycle);
 }
+
+void draw3D(){ }
 
 void draw2D(){
 
@@ -72,18 +75,17 @@ void draw2D(){
 
 	glPushMatrix();
 
-		glScalef(zoom, zoom, zoom);
+		glScalef(zoomCycle, zoomCycle, zoomCycle);
 
 		int iterations = 7;
 
 		// subdivisions of space, zooming will repeat on the center one
 		//    eg. ternary cantor set would be 3
 
-		int INTERVAL = 3; 
 		for(int i = 0; i < iterations; i++){
 			glPushMatrix();
 			float scale = powf(INTERVAL, i);
-			float color = (i-lvlPercent)/iterations;
+			float color = (i-linearCycle)/iterations;
 			glColor3f(color*0.75 + 0.25, color*0.75 + 0.25, color*0.75 + 0.25);
 			glScalef(1.0/scale, 1.0/scale, 1.0/scale);
 			repeating2DScene();
